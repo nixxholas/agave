82d9d1d01aa74340a3645932792583a920459b3a
Michael McGee <mmcgee@jumptrading.com>
2023-10-17 21:35:40 +0000

    firedancer: configure tvu port from arguments
    Firedancer determines the port for the TVU, which is specified in its
    config file, so an argument is added to the CLI and plumbed all the
    way through to gossip to wire this up.

--- validator/src/main.rs
+++ validator/src/main.rs
@@ -2025,14 +2025,19 @@ where
         .map(ContactInfo::new_gossip_entry_point)
         .collect::<Vec<_>>();
 
-    // FIREDANCER: Get TPU port from the CLI.
+    // FIREDANCER: Get application ports from the CLI.
     let firedancer_tpu_port = value_t_or_exit!(matches, "firedancer_tpu_port", u16);
+    let firedancer_tvu_port = value_t_or_exit!(matches, "firedancer_tvu_port", u16);
+
     let mut node = Node::new_with_external_ip(
         &identity_keypair.pubkey(),
         node_config,
         // FIREDANCER: Desired port for the TPU is passed in from the config file, so it
         // can be broadcast correctly via. gossip.
         firedancer_tpu_port,
+        // FIREDANCER: Desired port for the TVU is passed in from the config file, so it
+        // can be broadcast correctly via. gossip.
+        firedancer_tvu_port,
     );
 
     if restricted_repair_only_mode {
