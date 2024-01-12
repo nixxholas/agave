83cd293be61bed0b3071ca0896f2b57cea8c01d8
Michael McGee <mmcgee@jumptrading.com>
2024-01-12 22:38:51 +0000

    firedancer: use firedancer poh recorder

--- poh/src/lib.rs
+++ poh/src/lib.rs
@@ -1,6 +1,17 @@
 #![allow(clippy::arithmetic_side_effects)]
 pub mod leader_bank_notifier;
-pub mod poh_recorder;
+// FIREDANCER: Replace PohRecorder completely with one that goes out to
+// our implementation.
+// pub mod poh_recorder;
+mod firedancer_poh_recorder;
+mod old_poh_recorder;
+
+pub mod poh_recorder {
+  pub use crate::firedancer_poh_recorder::*;
+  // use everything except PohRecorder from old_poh_recorder
+  pub use crate::old_poh_recorder::{WorkingBankEntry, BankStart, PohRecorderError, RecordTransactionsSummary, RecordTransactionsTimings, TransactionRecorder, GRACE_TICKS_FACTOR, MAX_GRACE_SLOTS, PohLeaderStatus, Record };
+}
+
 pub mod poh_service;
 
 #[macro_use]
