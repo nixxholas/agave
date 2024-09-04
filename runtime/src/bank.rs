f70ab5598ccd86b216c3928e4397bf4a5b58d723
Manik Jain <mjain@jumptrading.com>
2024-09-04 23:24:09 +0000

    Make epoch schedule and rent updaters public

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1989,7 +1989,7 @@ impl Bank {
         self.epoch_stakes.insert(epoch, stakes);
     }
 
-    fn update_rent(&self) {
+    pub fn update_rent(&self) {
         self.update_sysvar_account(&sysvar::rent::id(), |account| {
             create_account(
                 &self.rent_collector.rent,
@@ -1998,7 +1998,7 @@ impl Bank {
         });
     }
 
-    fn update_epoch_schedule(&self) {
+    pub fn update_epoch_schedule(&self) {
         self.update_sysvar_account(&sysvar::epoch_schedule::id(), |account| {
             create_account(
                 self.epoch_schedule(),
