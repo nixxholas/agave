929494242679ae7e62fee3ba9ce6a8cccd8d1a71
Michael McGee <mmcgee@jumptrading.com>
2024-10-07 20:46:15 +0000

    firedancer: gui, send slot completed/rooted/optimistic messages
    Co-authored-by: Yufeng Zhou <yufzhou@jumptrading.com>

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -396,7 +396,7 @@ impl AddAssign for SquashTiming {
 
 #[cfg_attr(feature = "frozen-abi", derive(AbiExample))]
 #[derive(Debug, Default, PartialEq)]
-pub(crate) struct CollectorFeeDetails {
+pub struct CollectorFeeDetails {
     transaction_fee: u64,
     priority_fee: u64,
 }
@@ -921,7 +921,7 @@ pub struct Bank {
     collector_id: Pubkey,
 
     /// Fees that have been collected
-    collector_fees: AtomicU64,
+    pub collector_fees: AtomicU64,
 
     /// Track cluster signature throughput and adjust fee rate
     pub(crate) fee_rate_governor: FeeRateGovernor,
@@ -1002,7 +1002,7 @@ pub struct Bank {
     check_program_modification_slot: bool,
 
     /// Collected fee details
-    collector_fee_details: RwLock<CollectorFeeDetails>,
+    pub collector_fee_details: RwLock<CollectorFeeDetails>,
 
     /// The compute budget to use for transaction execution.
     compute_budget: Option<ComputeBudget>,
@@ -4210,6 +4210,8 @@ impl Bank {
         {
             self.filter_program_errors_and_collect_fee_details(&execution_results)
         } else {
+            /// FIREDANCER: GUI needs the full fee details to be filled in.
+            let _ = self.filter_program_errors_and_collect_fee_details(&execution_results);
             self.filter_program_errors_and_collect_fee(&execution_results)
         };
         update_transaction_statuses_time.stop();
